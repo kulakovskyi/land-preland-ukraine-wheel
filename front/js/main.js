9549c4e4a008e4ce8505a9c8a76d66f35ab3a543
kulakovskyi <torchok9@gmail.com>
2022-11-09 18:17:16 +0200

    add desc 1920 and two popUp

--- front/js/main.js
+++ front/js/main.js
@@ -3,31 +3,23 @@ const playBtn = document.querySelector('.bonus__main-wheel-btn'),
       main = document.querySelector('.bonus__main'),
       chips = document.querySelectorAll('.bonus__chip'),
       popupChips = document.querySelectorAll('.bonus__overlay-chip'),
-      firstWoman = document.querySelector('.bonus__overlay-firstWoman'),
-      secondWoman = document.querySelector('.bonus__overlay-secondWoman'),
+
       wheel = document.querySelector('.bonus__main-wheel-reel'),
       overlay = document.querySelector('.bonus__overlay'),
       popupFirst = document.querySelector('.bonus__firstWin'),
       popupFirstBtn = document.querySelector('.bonus__firstWin-btn'),
       popupSecond = document.querySelector('.bonus__secondWin'),
       overflow = document.querySelector('body'),
-      video = document.querySelector('.bonus__main-video-bg'),
-      wrapper = document.querySelector('.bonus')
+
+      wrapper = document.querySelector('.bonus'),
+      ellipseOne = document.querySelector('.bonus__main-wheel-ellipse-one'),
+      ellipseTwo = document.querySelector('.bonus__main-wheel-ellipse-two')
 
 
-const bonusText = document.querySelector('.bonus__main-bubble');
-
-window.onload = videoSource(video, 'img/video.mp4', 'video/mp4');
 
 
-function videoSource(element, src, type) {
-    if(window.innerWidth > 1024) {
-        let source = document.createElement('source')
-        source.src = src
-        source.type = type
-        video.appendChild(source)
-    }
-}
+
+
 
 let triesCounter = 0
 
@@ -44,6 +36,8 @@ playBtn.addEventListener('click', () => {
 function runFirstRotation() {
     wheel.classList.add('reel-rotation-first')
     playBtn.classList.remove('pulse-btn')
+    ellipseOne.classList.remove('pulse-btn')
+    ellipseTwo.classList.remove('pulse-btn')
     playBtn.style.cursor = 'default'
     wrapper.style.pointerEvents = 'none'
     setTimeout(() => {
@@ -57,20 +51,22 @@ function doAfterFirstRotation() {
     wheel.classList.remove('reel-rotation-first')
     overlay.classList.add('win-tab')
     overlay.classList.add('win-mob')
-    firstWoman.classList.remove('hide')
     displayPopup(popupFirst)
     wrapper.style.pointerEvents = 'auto'
     overflow.style.overflow = 'hidden'
     setTimeout(() => {
         playBtn.classList.add('pulse-btn')
+        ellipseOne.classList.add('pulse-btn')
+        ellipseTwo.classList.add('pulse-btn')
         playBtn.style.cursor = 'pointer'
-        bonusText.style.display = "none"
     }, 1200)
 }
 
 function runSecondRotation() {
     wheel.classList.add('reel-rotation-second')
     playBtn.classList.remove('pulse-btn')
+    ellipseOne.classList.remove('pulse-btn')
+    ellipseTwo.classList.remove('pulse-btn')
     playBtn.style.cursor = 'default'
     overflow.style.overflow = 'hidden'
     wrapper.style.pointerEvents = 'none'
@@ -83,7 +79,6 @@ function runSecondRotation() {
 function doAfterSecondRotation() {
     overlay.classList.add('win-tab')
     overlay.classList.add('win-mob')
-    secondWoman.classList.remove('hide')
     displayPopup(popupSecond)
     wrapper.style.pointerEvents = 'auto'
 }
@@ -91,7 +86,6 @@ function doAfterSecondRotation() {
 
 popupFirstBtn.addEventListener('click', () => {
     main.classList.remove('blur')
-    firstWoman.classList.add('hide')
     chips.forEach(function (el) {
         el.classList.remove('blur')
     })
@@ -114,58 +108,3 @@ function displayPopup(popup) {
     })
 }
 
-(function () {
-    let url = new URL(window.location.href)
-
-    const params = [
-        'l',
-        'utm_source',
-        'utm_medium',
-        'utm_campaign',
-        'utm_term',
-        'utm_content',
-        'param1',
-        'param2'
-    ]
-
-    const linkParams = [
-        'affid',
-        'cpaid'
-    ]
-
-    // ищем в url redirectUrl в url:
-    if (url.searchParams.has('redirectUrl')) {
-        var redirectUrl = new URL(url.searchParams.get('redirectUrl'));
-        if (redirectUrl.href.match(/\//g).length === 4 && redirectUrl.searchParams.get('l')) { //если ссылка в ссылка redirectUrl корректная
-            localStorage.setItem('redirectUrl', redirectUrl.href); // указываем точкой входа домен с протоколом из redirectUrl
-        }
-    }
-    /////////
-
-    params.forEach((param) => {
-        if (url.searchParams.has(param)) localStorage.setItem(param, url.searchParams.get(param))
-    })
-
-    linkParams.forEach((linkParam) => {
-        if (url.searchParams.has(linkParam)) localStorage.setItem(linkParam, url.searchParams.get(linkParam))
-    })
-
-    window.addEventListener('click', function (e) {
-        var link, parent = e.target.closest('a')
-        parent && (
-            e.preventDefault(),
-            localStorage.getItem("redirectUrl") ?
-            link = new URL(localStorage.getItem("redirectUrl"))
-            : (
-                link = new URL(parent.href),
-                affid = localStorage.getItem('affid'),
-                cpaid = localStorage.getItem('cpaid'),
-                affid && cpaid && (link.pathname = '/' + affid + '/' + cpaid)
-                ),
-
-            params.forEach((param) => {
-                url.searchParams.has(param) && link.searchParams.set(param, localStorage.getItem(param))
-            }
-        ), document.location.href = link)
-    })
-})()
